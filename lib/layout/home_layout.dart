bdc7f3a3c8e114d7e0268cf250fa79044ec4033b
Husseinhtm99 <husseinhtm99@gmail.com>
2022-12-19 00:30:33 +0200

    ** Hussein

--- lib/layout/home_layout.dart
+++ lib/layout/home_layout.dart
@@ -1,16 +1,12 @@
-// import 'package:flutter/material.dart';
-//
-//
-// class HomeLayout extends StatelessWidget {
-//
-//   const HomeLayout({Key? key}) : super(key: key);
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: defaultAppBar(),
-//       body: const Text('HOME LAYOUT'),
-//     );
-//   }
-// }
+import 'package:flutter/material.dart';
 
+class HomeLayout extends StatelessWidget {
+  const HomeLayout({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: const Text('HOME LAYOUT'),
+    );
+  }
+}
