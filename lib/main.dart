bd32aa4dacc4d628bddc6baa8691d7258332c90e
HusseinMohamed99 <husseinhtm99@gmail.com>
2023-02-10 15:31:42 +0200

    *Hussein phase {1} % update exploreScreen + wishlistScreen + profileScreen + splashScreen + InternetConnection % citiesScreen + searchScreen + filter + responsiveUi % updateAll Components % update All Widget + icon launcher

--- lib/main.dart
+++ lib/main.dart
@@ -82,6 +82,8 @@ class MyApp extends StatelessWidget {
             ]);
             return ScreenUtilInit(
                 designSize: const Size(360, 690),
+                minTextAdapt: true,
+                splitScreenMode: true,
                 builder: (context, child) {
                   return MaterialApp(
                     debugShowCheckedModeBanner: false,
