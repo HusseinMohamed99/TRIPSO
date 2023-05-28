c64ff539ff88983d63eba1d2aed08f1ae24f6b46
HusseinMohamed <husseinhtm99@gmail.com>
2023-05-29 02:39:42 +0300

    *Hussein phase {1} % update exploreScreen + wishlistScreen + profileScreen + update userData + historicalScreen + SightsScreen + uploadProfileImage + splashScreen + InternetConnection % citiesScreen + searchScreen + filter + responsiveUi % updateAll Components % update All Widget + icon launcher + deleteAccount + Rate + WishList + Google Maps + Add Toast $$$ Finish Tasks

--- lib/mobile/screens/sign_up/sign_up_screen.dart
+++ lib/mobile/screens/sign_up/sign_up_screen.dart
@@ -40,11 +40,11 @@ class SignUpScreen extends StatelessWidget {
           MyDialog.showLoadingDialog(context, 'SignUp is successfully');
         }
         if (state is UserCreateSuccessState) {
-          TripsoCubit.get(context).getUserData();
           showToast(text: 'Login Up Successfully', state: ToastStates.success);
           MyDialog.showLoadingDialog(context, 'SignUp is successfully');
           CacheHelper.saveData(value: state.uid, key: 'uId').then((value) {
             uId = state.uid;
+            TripsoCubit.get(context).getUserData();
             navigateAndFinish(context, routeName: CitiesScreen.routeName);
           });
         } else if (state is SignUpErrorState) {
