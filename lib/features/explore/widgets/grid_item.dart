d6972eea76a63da5879bea531f044a120afcab42
HusseinMohamed99 <husseinhtm99@gmail.com>
2024-11-28 14:32:10 +0200

    Refactor GridItemSights widget to improve readability and maintainability
    Extracted image, overlay, and place information into helper functions to clean up the build method and enhance code readability. Added comments for better understanding of each section.

--- lib/features/explore/widgets/grid_item.dart
+++ lib/features/explore/widgets/grid_item.dart
@@ -39,11 +39,13 @@ class GridItemSights extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    int rating = (double.tryParse(placeModel.rate) ?? 0)
-        .toInt(); // Parse rating from string to int
+    // Parse rating from string to int
+    int rating = (double.tryParse(placeModel.rate) ?? 0).toInt();
+
     return InkWell(
       borderRadius: BorderRadius.circular(20).r,
       onTap: () {
+        // Navigate to the place details page
         // navigateTo(
         //   context,
         //   routeName: SightDetailsScreen.routeName,
@@ -51,9 +53,6 @@ class GridItemSights extends StatelessWidget {
         //       placeModel: placeModel,
         //       cityModel: cityModel),
         // );
-        // if (kDebugMode) {
-        //   print(placeModel.pId);
-        // }
       },
       child: Stack(
         clipBehavior: Clip.antiAliasWithSaveLayer,
@@ -71,68 +70,9 @@ class GridItemSights extends StatelessWidget {
                   alignment: Alignment.bottomLeft,
                   clipBehavior: Clip.antiAliasWithSaveLayer,
                   children: [
-                    ClipRRect(
-                      borderRadius: BorderRadius.circular(12).r,
-                      child: CachedNetworkImage(
-                        imageUrl: placeModel.image,
-                        fit: BoxFit.cover,
-                        height: 220.h,
-                        width: double.infinity,
-                        progressIndicatorBuilder:
-                            (context, url, downloadProgress) => Center(
-                          child: AdaptiveIndicator(),
-                        ),
-                        errorWidget: (context, url, error) => Icon(
-                          Icons.info,
-                          size: 24.sp,
-                        ),
-                      ),
-                    ),
-                    LayerImage(
-                      height: 220.h,
-                      width: double.infinity,
-                      borderRadius: BorderRadius.circular(12).r,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(10.0).r,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            placeModel.name.trim(),
-                            maxLines: 2,
-                            overflow: TextOverflow.ellipsis,
-                            style:
-                                Theme.of(context).textTheme.bodySmall?.copyWith(
-                                      color: ColorsManager.whiteColor,
-                                    ),
-                          ),
-                          Row(
-                            children: generateRatingIcons(rating),
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            children: [
-                              Text(
-                                TripsoCubit.get(context).cityModel!.country,
-                                style: Theme.of(context)
-                                    .textTheme
-                                    .headlineSmall
-                                    ?.copyWith(
-                                      color: ColorsManager.whiteColor,
-                                    ),
-                              ),
-                              Space(height: 0.h, width: 10.w),
-                              Icon(
-                                Icons.location_on_outlined,
-                                color: ColorsManager.primaryColor,
-                                size: 25.sp,
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
+                    _buildImage(placeModel.image),
+                    _buildOverlay(),
+                    _buildPlaceInfo(context, rating),
                   ],
                 ),
               ],
@@ -140,6 +80,90 @@ class GridItemSights extends StatelessWidget {
           ),
         ],
       ),
+    );
+  }
+
+  // Helper function to build the image with a loading indicator and error widget
+  Widget _buildImage(String imageUrl) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(12).r,
+      child: CachedNetworkImage(
+        imageUrl: imageUrl,
+        fit: BoxFit.cover,
+        height: 220.h,
+        width: double.infinity,
+        progressIndicatorBuilder: (context, url, downloadProgress) => Center(
+          child: AdaptiveIndicator(),
+        ),
+        errorWidget: (context, url, error) => Icon(
+          Icons.info,
+          size: 24.sp,
+        ),
+      ),
+    );
+  }
+
+  // Helper function to build the overlay layer
+  Widget _buildOverlay() {
+    return LayerImage(
+      height: 220.h,
+      width: double.infinity,
+      borderRadius: BorderRadius.circular(12).r,
+    );
+  }
+
+  // Helper function to build the place information text and rating row
+  Widget _buildPlaceInfo(BuildContext context, int rating) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0).r,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          _buildPlaceName(context),
+          _buildRatingRow(rating),
+          _buildLocationRow(context),
+        ],
+      ),
+    );
+  }
+
+  // Helper function to build the place name text
+  Widget _buildPlaceName(BuildContext context) {
+    return Text(
+      placeModel.name.trim(),
+      maxLines: 2,
+      overflow: TextOverflow.ellipsis,
+      style: context.labelLarge?.copyWith(
+        color: ColorsManager.whiteColor,
+      ),
+    );
+  }
+
+  // Helper function to build the rating row with stars
+  Widget _buildRatingRow(int rating) {
+    return Row(
+      children: generateRatingIcons(rating),
+    );
+  }
+
+  // Helper function to build the location row with country and icon
+  Widget _buildLocationRow(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        Text(
+          TripsoCubit.get(context).cityModel!.country,
+          style: Theme.of(context).textTheme.headlineSmall?.copyWith(
+                color: ColorsManager.whiteColor,
+              ),
+        ),
+        Space(height: 0.h, width: 10.w),
+        Icon(
+          Icons.location_on_outlined,
+          color: ColorsManager.primaryColor,
+          size: 25.sp,
+        ),
+      ],
     );
   }
 }
