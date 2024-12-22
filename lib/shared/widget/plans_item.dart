4319b1c1f783ce388b7a29ecde2ac6b7e0c7b191
HusseinMohamed99 <husseinhtm99@gmail.com>
2024-12-22 14:34:19 +0200

    Replace InkWell with GestureDetector in TopPlanItem
    Replaced InkWell with GestureDetector to remove the unnecessary borderRadius property and cleaned up the padding application by using an extension method. Adjusted the text style to use `context.titleSmall` for better consistency.

--- lib/shared/widget/plans_item.dart
+++ lib/shared/widget/plans_item.dart
@@ -14,8 +14,7 @@ class TopPlanItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      borderRadius: BorderRadius.circular(20).r,
+    return GestureDetector(
       onTap: () async {
         // navigateTo(
         //   context,
@@ -29,40 +28,41 @@ class TopPlanItem extends StatelessWidget {
         //   print(bestPLanModel.pId);
         // }
       },
-      child: Padding(
-        padding: const EdgeInsets.only(left: 16, right: 16).r,
-        child: Stack(
-          alignment: Alignment.center,
-          children: [
-            Container(
-              alignment: Alignment.bottomLeft,
-              clipBehavior: Clip.antiAliasWithSaveLayer,
-              height: 200.h,
-              width: 220.w,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.all(
-                  const Radius.circular(20).r,
-                ),
-                image: const DecorationImage(
-                  fit: BoxFit.fitHeight,
-                  image: AssetImage('assets/images/2021884.jpg'),
-                ),
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          Container(
+            alignment: Alignment.bottomLeft,
+            clipBehavior: Clip.antiAliasWithSaveLayer,
+            height: 200.h,
+            width: 220.w,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(
+                const Radius.circular(20).r,
+              ),
+              image: const DecorationImage(
+                fit: BoxFit.fitHeight,
+                image: AssetImage('assets/images/2021884.jpg'),
               ),
             ),
-            LayerImage(
-              height: 200.h,
-              width: 220.w,
+          ),
+          LayerImage(
+            height: 200.h,
+            width: 220.w,
+          ),
+          Text(
+            bestPLanModel.name,
+            textAlign: TextAlign.center,
+            style: context.titleSmall?.copyWith(
+              color: ColorsManager.blackPrimary,
+              fontWeight: FontWeightHelper
+                  .semiBold, // Ensure FontWeightHelper is defined
             ),
-            Text(
-              bestPLanModel.name,
-              textAlign: TextAlign.center,
-              style: context.titleLarge?.copyWith(
-                color: ColorsManager.blackPrimary,
-                fontWeight: FontWeightHelper.semiBold,
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
+      ).onlyPadding(
+        leftPadding: 16,
+        rightPadding: 16,
       ),
     );
   }
